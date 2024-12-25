027c7aefaf2075338b22f145d390fd465a0341e7
rveach <rveach@gmail.com>
2024-12-25 14:57:59 -0600

    wip: throwing down converter thoughts

--- src/bedrock_mapper_libs/__init__.py
+++ src/bedrock_mapper_libs/__init__.py
@@ -1,4 +1,5 @@
-# import os
+import os
+import subprocess
 
 from typing import Self
 
@@ -9,6 +10,24 @@ class BedrockMapper(ScriptBase):
 
     PARSER_VERBOSITY_CONFIG = "count"
 
+    BLUEMAP_CLI_JAR = os.getenv("BLUEMAP_CLI_JAR")
+    CHUNKER_CLI_JAR=os.getenv("CHUNKER_CLI_JAR")
+
+    def convert_world(self: Self, src_dir: str, dest_dir: str, dest_format: str):
+
+        cmd = [
+            "java",
+            "-jar",
+            self.CHUNKER_CLI_JAR,
+            "-i",
+            src_dir,
+            "-f",
+            dest_format,
+            "-o",
+            dest_dir
+        ]
+
+        subprocess.run(cmd)
 
     def runJob(self: Self):
         pass
